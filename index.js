60fc37d2f85cadcabfe8830eb4c93d1bb3a48ec4
zach balder <zmbalder@gmail.com>
2022-02-04 13:08:01 -0500

    more sensible config structure

--- index.js
+++ index.js
@@ -2,15 +2,9 @@
 
 const isObjectLiteral = x => (x.constructor || {}).name === 'Object'
 
-/**
- * @param  {Object} config
- * @param  {Object} options
- *
- * @return {Function}
- */
 const reworder = (config, options = {}) => {
-  if (!isObjectLiteral(config)) {
-    throw new Error('Expected config to be an object literal')
+  if (!Array.isArray(config) || !config.every(isObjectLiteral)) {
+    throw new Error('Expected config to be an array of object literals')
   }
 
   if (!isObjectLiteral(options)) {
@@ -24,7 +18,7 @@ const reworder = (config, options = {}) => {
     throw new Error('Expected options.caseInsensitive to be a boolean')
   }
 
-  const keyInfos = []
+  const infos = []
   const pattern = []
   const patterns = []
   const regexOpts = 'g' + (options.caseInsensitive ? 'i' : '')
@@ -33,79 +27,84 @@ const reworder = (config, options = {}) => {
   let isRegex
   let regex
 
-  for (const key in config) {
-    const values = [].concat(config[key])
+  for (let { key, keys, value } of config) {
+    if (typeof value !== 'string') {
+      throw new Error('Config value must be string')
+    }
 
-    for (let value of values) {
-      if (value instanceof RegExp) {
+    keys = [].concat(key).concat(keys).filter(Boolean)
+
+    for (let key of keys) {
+      if (key instanceof RegExp) {
         isRegex = true
-        value = value.source.replace(/\(/g, '(?:')
-      } else if (typeof value === 'string') {
+        key = key.source.replace(/\(/g, '(?:')
+      } else if (typeof key === 'string') {
         isRegex = false
       } else {
-        throw new Error('Config value must be string or RegExp')
+        throw new Error('Config key must be string or RegExp')
       }
 
-      regex = new RegExp(`^${value}$`, regexOpts)
+      regex = new RegExp(`^${key}$`, regexOpts)
 
       for (const pattern of patterns) {
         const isConflict = (
-          (pattern.isRegex && pattern.regex.test(value)) ||
-          (isRegex && regex.test(pattern.value))
+          (pattern.isRegex && pattern.regex.test(key)) ||
+          (isRegex && regex.test(pattern.key))
         )
 
         if (isConflict) {
-          const patternValue = pattern.isRegex ? `/${pattern.value}/` : `"${pattern.value}"`
-          const thisValue = isRegex ? `/${value}/` : `"${value}"`
+          const patternKey = pattern.isRegex ? `/${pattern.key}/` : `"${pattern.key}"`
+          const thisKey = isRegex ? `/${key}/` : `"${key}"`
 
           throw new Error(
-            `Conflict: {${patternValue}: "${pattern.key}"}, {${thisValue}: "${key}"}`
+            `Conflict: {${patternKey}: "${pattern.value}"}, {${thisKey}: "${value}"}`
           )
         }
       }
 
-      pattern.push(`(\\b${value}\\b)`)
+      pattern.push(`(\\b${key}\\b)`)
       patterns.push({ isRegex, key, regex, value })
 
       ++index
     }
 
-    keyInfos.push({ key, index })
+    infos.push({ value, index })
   }
 
   const lastIndex = ++index
   regex = new RegExp(pattern.join('|'), regexOpts)
 
-  const getKey = index => {
+  const getValue = index => {
     let i
 
-    for (i = 0; i < keyInfos.length && keyInfos[i].index <= index; i++) {
-      if (keyInfos[i].index === index) break
+    for (i = 0; i < infos.length && infos[i].index <= index; i++) {
+      if (infos[i].index === index) break
     }
 
-    return keyInfos[i].key
+    return infos[i].value
   }
 
   const reword = input => {
     const matches = []
 
+    let index
     let match
     let net = 0
     let output = input
 
     while ((match = regex.exec(input))) {
-      const keyIndex = match.slice(1, lastIndex).findIndex(Boolean) + 1
-      const key = getKey(keyIndex)
-      const index = match.index - net
+      index = match.slice(1, lastIndex).findIndex(Boolean) + 1
+      const value = getValue(index)
+      index = match.index - net
 
       output = (
         output.slice(0, index) +
-        key +
+        value +
         output.slice(index + match[0].length)
       )
 
-      matches.push({ match: match[0], index: match.index, key })
-      net += match[0].length - key.length
+      matches.push({ key: match[0], index: match.index, value })
+      net += match[0].length - value.length
     }
 
     return { input, matches, output }
--- test.js
+++ test.js
@@ -6,18 +6,27 @@ const assert = require('assert')
 const reworder = require('.')
 
 describe('reworder', () => {
-  it('throws if config isn\'t object literal', () => {
+  it('throws if config isn\'t array', () => {
     try {
-      reworder(Object.create(null))
+      reworder({})
       assert.fail('Should throw')
     } catch (err) {
-      assert.strictEqual(err.message, 'Expected config to be an object literal')
+      assert.strictEqual(err.message, 'Expected config to be an array of object literals')
+    }
+  })
+
+  it('throws if config contains non-object literal', () => {
+    try {
+      reworder([1])
+      assert.fail('Should throw')
+    } catch (err) {
+      assert.strictEqual(err.message, 'Expected config to be an array of object literals')
     }
   })
 
   it('throws if options isn\'t object literal', () => {
     try {
-      reworder({}, Object.create(null))
+      reworder([], Object.create(null))
       assert.fail('Should throw')
     } catch (err) {
       assert.strictEqual(err.message, 'Expected options to be an object literal')
@@ -26,33 +35,47 @@ describe('reworder', () => {
 
   it('throws if options.caseInsensitive isn\'t boolean', () => {
     try {
-      reworder({}, { caseInsensitive: 1 })
+      reworder([], { caseInsensitive: 1 })
       assert.fail('Should throw')
     } catch (err) {
       assert.strictEqual(err.message, 'Expected options.caseInsensitive to be a boolean')
     }
   })
 
-  it('throws if config value isn\'t string or regex', () => {
+  it('throws if config key isn\'t string or RegExp', () => {
     try {
-      reworder({ bar: Symbol('foo') })
+      reworder([{ key: 1, value: 'bar' }])
       assert.fail('Should throw')
     } catch (err) {
-      assert.strictEqual(err.message, 'Config value must be string or RegExp')
+      assert.strictEqual(err.message, 'Config key must be string or RegExp')
+    }
+  })
+
+  it('throws if config value isn\'t string', () => {
+    try {
+      reworder([{ key: 'foo', value: Symbol('bar') }])
+      assert.fail('Should throw')
+    } catch (err) {
+      assert.strictEqual(err.message, 'Config value must be string')
     }
   })
 
   it('replaces words', () => {
     const input = 'foo bar baz bam hello world'
-    const reword = reworder({ bar: 'foo', foo: 'bar' })
+
+    const reword = reworder([
+      { key: 'foo', value: 'bar' },
+      { key: 'bar', value: 'foo' }
+    ])
+
     const result = reword(input)
 
     assert.deepStrictEqual(result, {
       input,
 
       matches: [
-        { match: 'foo', index: 0, key: 'bar' },
-        { match: 'bar', index: 4, key: 'foo' }
+        { key: 'foo', index: 0, value: 'bar' },
+        { key: 'bar', index: 4, value: 'foo' }
       ],
 
       output: 'bar foo baz bam hello world'
@@ -62,10 +85,10 @@ describe('reworder', () => {
   it('replaces words (case insensitive)', () => {
     const input = 'foo bAr baz BaM hello world'
 
-    const reword = reworder(
-      { bar: 'foo', foo: 'bar' },
-      { caseInsensitive: true }
-    )
+    const reword = reworder([
+      { key: 'foo', value: 'bar' },
+      { key: 'bar', value: 'foo' }
+    ], { caseInsensitive: true })
 
     const result = reword(input)
 
@@ -73,8 +96,8 @@ describe('reworder', () => {
       input,
 
       matches: [
-        { match: 'foo', index: 0, key: 'bar' },
-        { match: 'bAr', index: 4, key: 'foo' }
+        { key: 'foo', index: 0, value: 'bar' },
+        { key: 'bAr', index: 4, value: 'foo' }
       ],
 
       output: 'bar foo baz BaM hello world'
@@ -84,11 +107,11 @@ describe('reworder', () => {
   it('replaces words and phrase', () => {
     const input = 'foo bar baz bam hello world'
 
-    const reword = reworder({
-      bar: 'foo',
-      foo: 'bar',
-      helloworld: 'hello world'
-    })
+    const reword = reworder([
+      { key: 'foo', value: 'bar' },
+      { key: 'bar', value: 'foo' },
+      { key: 'hello world', value: 'helloworld' }
+    ])
 
     const result = reword(input)
 
@@ -96,9 +119,9 @@ describe('reworder', () => {
       input,
 
       matches: [
-        { match: 'foo', index: 0, key: 'bar' },
-        { match: 'bar', index: 4, key: 'foo' },
-        { match: 'hello world', index: 16, key: 'helloworld' }
+        { key: 'foo', index: 0, value: 'bar' },
+        { key: 'bar', index: 4, value: 'foo' },
+        { key: 'hello world', index: 16, value: 'helloworld' }
       ],
 
       output: 'bar foo baz bam helloworld'
@@ -107,16 +130,16 @@ describe('reworder', () => {
 
   it('replaces with regex', () => {
     const input = 'foo bar baz bam hello world'
-    const reword = reworder({ foo: /ba\S*/ })
+    const reword = reworder([{ key: /ba\S*/, value: 'foo' }])
     const result = reword(input)
 
     assert.deepStrictEqual(result, {
       input,
 
       matches: [
-        { match: 'bar', index: 4, key: 'foo' },
-        { match: 'baz', index: 8, key: 'foo' },
-        { match: 'bam', index: 12, key: 'foo' }
+        { key: 'bar', index: 4, value: 'foo' },
+        { key: 'baz', index: 8, value: 'foo' },
+        { key: 'bam', index: 12, value: 'foo' }
       ],
 
       output: 'foo foo foo foo hello world'
@@ -125,15 +148,15 @@ describe('reworder', () => {
 
   it('replaces multiple words with single word', () => {
     const input = 'foo bar baz bam hello world'
-    const reword = reworder({ foo: ['bar', 'bam'] })
+    const reword = reworder([{ keys: ['bar', 'bam'], value: 'foo' }])
     const result = reword(input)
 
     assert.deepStrictEqual(result, {
       input,
 
       matches: [
-        { match: 'bar', index: 4, key: 'foo' },
-        { match: 'bam', index: 12, key: 'foo' }
+        { key: 'bar', index: 4, value: 'foo' },
+        { key: 'bam', index: 12, value: 'foo' }
       ],
 
       output: 'foo foo baz foo hello world'
@@ -142,15 +165,15 @@ describe('reworder', () => {
 
   it('replaces with regex with subgroups', () => {
     const input = 'foo baz bam bit hello world'
-    const reword = reworder({ foo: /b(az|it)*/ })
+    const reword = reworder([{ key: /b(az|it)*/, value: 'foo' }])
     const result = reword(input)
 
     assert.deepStrictEqual(result, {
       input,
 
       matches: [
-        { match: 'baz', index: 4, key: 'foo' },
-        { match: 'bit', index: 12, key: 'foo' }
+        { key: 'baz', index: 4, value: 'foo' },
+        { key: 'bit', index: 12, value: 'foo' }
       ],
 
       output: 'foo foo bam foo hello world'
@@ -159,7 +182,11 @@ describe('reworder', () => {
 
   it('handles conflicting values (existing)', () => {
     try {
-      reworder({ bar: /fo\w*?/, baz: 'foo' })
+      reworder([
+        { key: /fo\w*?/, value: 'bar' },
+        { key: 'foo', value: 'baz' }
+      ])
+
       assert.fail('Should throw')
     } catch (err) {
       assert.strictEqual(err.message, 'Conflict: {/fo\\w*?/: "bar"}, {"foo": "baz"}')
@@ -168,7 +195,11 @@ describe('reworder', () => {
 
   it('handles conflicting values (new)', () => {
     try {
-      reworder({ bar: 'foo', baz: /fo\w*?/ })
+      reworder([
+        { key: 'foo', value: 'bar' },
+        { key: /fo\w*?/, value: 'baz' }
+      ])
+
       assert.fail('Should throw')
     } catch (err) {
       assert.strictEqual(err.message, 'Conflict: {"foo": "bar"}, {/fo\\w*?/: "baz"}')
@@ -176,10 +207,16 @@ describe('reworder', () => {
   })
 
   it('doesn\'t throw on overlapping values', () => {
-    reworder({ bar: 'foo', baz: 'foofoo' })
+    reworder([
+      { key: 'foo', value: 'bar' },
+      { key: 'foofoo', value: 'baz' }
+    ])
   })
 
   it('doesn\'t throw on matching values that don\'t line up', () => {
-    reworder({ bar: /fo./, baz: 'abcfoo' })
+    reworder([
+      { key: /fo./, value: 'bar' },
+      { key: 'abcfoo', value: 'baz' }
+    ])
   })
 })
